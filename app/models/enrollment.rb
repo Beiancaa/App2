e6d97c2f62e5838ee936dccefb3142afa4c7268c
beiancaa <beiancayousif89@gmail.com>
2022-10-17 19:34:36 +0300

    B-HW1-point4

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -4,4 +4,15 @@ class Enrollment < ApplicationRecord
 
   validates :date, presence: true
   
+  after_destroy :open_subject
+
+  private
+
+  def open_subject
+    if subject.open == false
+      subject.update_attribute :open, "true"
+      subject.save
+    end
+  end
+
 end

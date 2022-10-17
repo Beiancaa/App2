001dd9878715183517b173b28952ccb31c77c4ad
beiancaa <beiancayousif89@gmail.com>
2022-10-17 22:06:35 +0300

    B-HW1-point3

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,10 +3,18 @@ class Enrollment < ApplicationRecord
   belongs_to :subject
 
   validates :date, presence: true
-  
+
+  after_create :Check_subject_capacity
   after_destroy :open_subject
 
   private
+
+  def Check_subject_capacity
+    if subject.enrollments.count == subject.capacity
+      subject.update_attribute :open, "false"
+      subject.save
+    end
+  end
 
   def open_subject
     if subject.open == false
